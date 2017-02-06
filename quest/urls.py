e19e6b176a679747404b3040061783c414ae0b46
Vladyslav Vasyliev <vavlvl@mail.ru>
2017-02-06 04:29:46 +0300

    First commit

---
+++ quest/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+from django.template.backends import django
+
+from Survey import settings
+
+media_url = settings.MEDIA_URL.lstrip('/').rstrip('/')
+from quest import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='home'),
+    url(r'^survey/(?P<id>\d+)/$', views.survey_detail, name='survey_detail'),
+    url(r'^confirm/(?P<uuid>\w+)/$', views.confirm, name='confirmation'),
+    url(r'^privacy/$', views.privacy, name='privacy_statement'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+]
